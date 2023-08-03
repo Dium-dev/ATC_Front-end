1518426ae5dd5b9605a7a59e2c17472a2e01f3e4
anmedina-arg <andres.medina.arg@gmail.com>
2023-08-03 17:49:41 -0300

    initial setup for theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    colors: {
+      "primary-lm": "#8C0303",
+      "secondary-lm": "#F20505",
+      "background-lm": "#222222",
+      "text-lm": "#333333",
+      "primary-dm": "#444444",
+      "secondary-dm": "#555555",
+      "background-dm": "#666666",
+      "text-dm": "#f1f1f1",
+    },
+    screen: {
+      xs: "480px",
+      ms: "768px",
+      md: "990px",
+      lg: "1200px",
+      xl: "1920px",
+    },
     extend: {},
   },
   plugins: [],
