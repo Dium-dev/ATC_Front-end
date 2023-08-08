3380cbafc56a13769de8910e72d085b969e5ce3f
Clay Martinez <clay.ms@hotmail.com>
2023-08-08 17:09:51 -0500

    component button, modifidied

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image';
+import ButtonCarrete from "@/components/buttonCarrete/button";
+import Image from "next/image";
 
 export default function Home() {
-  return <p>Hola soy landing</p>;
+  return (
+    <>
+      <p>Hola soy landing</p>
+      <ButtonCarrete type="search" text="" />
+    </>
+  );
 }
