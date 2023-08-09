47db317026dba7fbfdce5aae3d9df87909d13ae4
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-08-09 17:09:00 -0300

    navbar

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -6,35 +6,14 @@ import ButtonNav from './buttonNav';
 import ProfileButton from './profileButton';
 import MobileMenu from './mobileMenu';
 import ButtonComponent from '../button/button';
-
-const navigation = [
-  {
-    label: 'Mi perfil',
-    route: '/dashboardUser',
-  },
-  {
-    label: 'Inicio',
-    route: '/',
-  },
-  {
-    label: 'Productos',
-    route: '/products',
-  },
-  {
-    label: 'Nosotros',
-    route: '/aboutUs',
-  },
-  {
-    label: 'Contacto',
-    route: '/contact',
-  },
-];
+import { FaAtlas, FaUser } from "react-icons/fa"
+import {BsCartFill} from "react-icons/bs"
 
 export const NavBar = () => {
   return (
   
-  <div className='h-fit w-[1080px] w-fit min-w-[800px] flex flex-row justify-between items-center bg-text-dm'>
-      <div className='flex flex-row items-center' >
+  <div className='h-fit w-full w-fit min-w-[800px] flex flex-row justify-between items-center bg-text-dm'>
+      <div className='flex flex-row items-center space-x-8' >
             <Image
                   className="h-18 w-18"
                   src={logo}
@@ -43,43 +22,22 @@ export const NavBar = () => {
                   // height={100}
         />
         <ButtonComponent type="white" text='Categorías' className="px-4"/>
-        <ButtonComponent type="white" text='Marca' />
-        <ButtonComponent type="white" text='Precio'/>
-    </div>
-      <div>
+        <ButtonComponent type="white" text='Marca' className="px-4" />
         
     </div>
-    <div></div>
+      <div className='flex flex-row min-w-[500px] bg-secondary-lm'>
+        <input type="text" className='w-full' />
+    </div>
+      <div className='flex flex-row space-x-8 items-center' >
+        <ButtonComponent type="white" text='Acerca de nosotros'/>
+        <ButtonComponent type="white" text='Contacto'/>
+        <FaAtlas className="cursor-pointer" />
+        <FaUser className="cursor-pointer"/>
+        <BsCartFill className="cursor-pointer"/>
+    </div>
   
   </div>
     
-    // <div className="min-h-full bg-background-dm">
-    //   <nav className="bg-gray-800">
-    //     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    //       <div className="flex h-16 items-center justify-center ">
-    //         <div className="flex items-center">
-    //           <div className="flex-shrink-0">
-    //             <Image
-    //               className="h-18 w-18"
-    //               src={logo}
-    //               alt="Your Company"
-    //               width={150}
-    //               // height={100}
-    //             />
-    //           </div>
-    //           <div className="hidden md:block">
-    //             <div className="ml-10 flex items-baseline space-x-4">
-    //               {navigation.map(({ label, route }) => (
-    //                 <ButtonNav key={route} label={label} route={route} />
-    //               ))}
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <ProfileButton />
-    //         < MobileMenu navigation ={navigation} />
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
+  
   );
 };
