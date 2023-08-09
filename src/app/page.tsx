556c017ab4b2e544607ecdcac5f74393b511e3bb
Clay Martinez <clay.ms@hotmail.com>
2023-08-09 14:35:24 -0500

    Removal of import and use of FilterBar component from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import FilterBar from "@/components/filterBar/filterBar";
-
 export default function Home() {
   return (
     <>
       <p>Hola soy landing</p>
-      <FilterBar />
     </>
   );
 }
