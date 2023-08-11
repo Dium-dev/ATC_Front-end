f913e4372d7aeb326f108b6867b08587c79bad24
Clay Martinez <clay.ms@hotmail.com>
2023-08-11 13:29:33 -0500

    button component + Link + props button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,27 +1,45 @@
+import Link from "next/link";
 import React from "react";
 
-interface ButtonComponentProps {
-  type?: string;
+type ButtonComponentProps = {
+  variant?: string;
   text?: string;
-}
+  to?: string;
+} & React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
-const ButtonComponent: React.FC<ButtonComponentProps> = ({ type, text }) => {
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  variant,
+  text,
+  to,
+  ...props
+}) => {
   const buttonClass =
-    type === "white"
+    variant === "white"
       ? "bg-white text-background-lm text-xs hover:text-background-dm hover:text-opacity-40"
-      : type === "red"
+      : variant === "red"
       ? "bg-secondary-lm text-xs hover:text-primary-lm"
-      : type === "search"
+      : variant === "search"
       ? "bg-secondary-lm text-text-dm text-sm hover:text-primary-lm"
       : "bg-white text-background-lm text-xs hover:text-background-dm hover:text-opacity-40";
 
-  const textType = type === "search" ? "Buscar" : "Boton";
+  const textvariant = variant === "search" ? "Buscar" : "Boton";
+
+  const StylesDefaul = ` min-w-[4.34rem] max-w-[8.6875rem] min-h-2.75[rem] max-h-[2.75rem] p-2 flex justify-center items-center font-bold ${buttonClass}`;
+
+  if (to) {
+    return (
+      <Link href={to} className={StylesDefaul}>
+        <p className="truncate"> {text || textvariant} </p>
+      </Link>
+    );
+  }
 
   return (
-    <button
-      className={` w-max-[8.6875rem] h-max-[2.75rem] p-2 flex justify-center items-center font-bold ${buttonClass}`}
-    >
-      <p className="truncate"> {text || textType} </p>
+    <button className={StylesDefaul} {...props}>
+      <p className="truncate"> {text || textvariant} </p>
     </button>
   );
 };
