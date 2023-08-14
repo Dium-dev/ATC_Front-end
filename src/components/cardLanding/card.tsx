25e68cd7aa32747bffa7da24e90ffe8e3bc43b49
Clay Martinez <clay.ms@hotmail.com>
2023-08-13 20:07:01 -0500

    component done

--- src/components/cardLanding/card.tsx
+++ src/components/cardLanding/card.tsx
@@ -2,47 +2,39 @@ import React, { FC } from "react";
 import Image from "next/image";
 import ButtonComponent from "../button/button";
 
-interface CardProps {}
+interface CardProps {
+  title: string;
+  price: string;
+  nota: string;
+  imageSrc: string;
+}
 
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc: "/img/Actualizatucarro_Facebook.jpg",
-    imageAlt: "Image product",
-    price: "$35",
-    color: "Black",
-  },
-];
-// 227 + 268
-
-const Card: FC<CardProps> = () => {
+const Card: FC<CardProps> = ({ title, price, nota, imageSrc }) => {
   return (
-    <div className="flex flex-col w-1/2 max-w-sm  pb-3 gap-2  border border-background-lm rounded-xl md:w-[14.1875rem] md:h-[268px] ">
+    <div className="flex flex-col max-w-[15.3125rem] max-h-[268px] pb-1 border border-background-lm   ">
       <Image
-        src={products[0].imageSrc}
+        src={imageSrc}
         alt="Cubre Volante"
-        width={227}
+        width={245}
         height={154}
-        className="w-full h-[154px] rounded-t-xl "
+        className="w-full h-[9.625rem]  "
       />
 
-      <div className=" p-[0.625rem] w-full  overflow-hidden">
-        <h3 className="font-oswald font-bold text-sm text-background-lm  ">
-          Cubre Volante Alta Calidad Cuerina Cocido Negro
+      <div className=" p-[0.625rem] w-full  h-[57px] overflow-hidden flex items-center justify-center">
+        <h3 className="font-oswald font-bold text-sm text-background-lm text-center line-clamp-2 ">
+          {title}
         </h3>
       </div>
 
-      <div className="w-full  overflow-hidden">
-        <p className="font-oswald font-bold text-xl text-background-lm text-center ">
-          $ 29500
+      <div className="w-full  overflow-hidden h-[33px] pb-2">
+        <p className="font-oswald font-bold text-xl text-background-lm text-center line-clamp-1">
+          {`$ ${price}`}
         </p>
       </div>
 
-      <div className="flex justify-between items-center px-[10px] pb-[5px] w-full  overflow-hidden">
-        <p className=" font-oswald font-bold text-xs text-background-lm ">
-          Performance
+      <div className="flex justify-between items-center px-[10px] pb-[5px] w-full h-[27px] gap-2 overflow-hidden">
+        <p className=" font-oswald font-bold text-xs text-background-lm w-1/3  overflow-hidden ">
+          {nota}
         </p>
         <ButtonComponent variant="red" text="Añadir al carrito" />
       </div>
