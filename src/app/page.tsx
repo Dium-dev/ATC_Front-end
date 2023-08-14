0358dcb8215b65dfafe1a2496ca617f708238be9
anmedina-arg <andres.medina.arg@gmail.com>
2023-08-14 08:47:00 -0300

    provando el primer icono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import icon from '../components/icons/icon1.svg'
+import Image from 'next/image';
 export default function Home() {
-  return <></>;
+  return <>
+    <Image src={icon} width={100} height={100} alt='icono de prueba 1' />
+  </>;
 }
