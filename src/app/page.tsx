5989728ab0495ff654bd1b50dcf44bec55dd3770
Clay Martinez <110847258+claymartinez@users.noreply.github.com>
2023-08-13 20:19:14 -0500

    Update page.tsx
    Elimine lo que modifique en page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import Card from "@/components/cardLanding/card";
-
 export default function Home() {
-  return (
-    <>
-      <div className=" flex justify-center items-center gap-2 px-5 ">
-        <Card />
-        <Card />
-      </div>
-    </>
-  );
+  return <></>;
 }
