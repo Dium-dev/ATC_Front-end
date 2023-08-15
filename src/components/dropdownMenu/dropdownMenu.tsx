f79996ecf3f3edd386f60cdc3a5147b75587ef8f
anmedina-arg <andres.medina.arg@gmail.com>
2023-08-15 16:01:00 -0300

    se corrigio el titulo del dropDown menu para que sea un span y no un componente anchor como estaba

--- src/components/dropdownMenu/dropdownMenu.tsx
+++ src/components/dropdownMenu/dropdownMenu.tsx
@@ -1,17 +1,17 @@
-"use-client";
-import { useState } from "react";
-import { FunctionComponent, ReactNode } from "react";
-import Link from "next/link";
+'use-client';
+import { useState } from 'react';
+import { FunctionComponent, ReactNode } from 'react';
+import Link from 'next/link';
 
 type AnchorProps = {
   title: string;
   to?: string;
 };
 
-export const Anchor: React.FC<AnchorProps> = ({ title, to="" }) => {
+export const Anchor: React.FC<AnchorProps> = ({ title, to }) => {
   return (
     <span className="w-full block p-2 hover:bg-primary-lm">
-      <Link href={to} target="_blank">
+      <Link href={`${to}`} target="_blank">
         {title}
       </Link>
     </span>
@@ -51,11 +51,13 @@ export const DropDownMenu: React.FC<DropDownMenuProps> = ({
       onMouseLeave={closeHoverMenu}
       onClick={clickHandleMenu}
     >
-      <Anchor title={title} />
+      <span className="w-full block p-2 hover:bg-primary-lm rounded-md">
+        {title}
+      </span>
       <ul
         id="lista"
         className={`flex flex-col items-center mt-4 border-2 border-solid border-secondary-dm rounded-md ${
-          hoverOpenMenu ? "hidden" : clickOpenMenu ? "visible" : "hidden"
+          hoverOpenMenu ? 'hidden' : clickOpenMenu ? 'visible' : 'hidden'
         }`}
       >
         {anchorArray.map(({ title, to }) => (
