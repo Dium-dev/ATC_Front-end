373091fc8ef96541f92d6d1e6afa591db947257b
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-21 18:30:58 -0300

    feat: add semicolon in component AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,48 +1,55 @@
-'use client'
-import { createContext, useContext, useEffect, useState } from "react";
-import { onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth'
-import { auth } from "~/config/firebase-config";
-const AuthContext = createContext<any>({})
+'use client';
+import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { auth } from '~/config/firebase-config';
+const AuthContext = createContext<any>({});
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);
 
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
-export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
-    const [user, setUser] = useState<any>(null);
-    const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const unsusbscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+        });
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+    return () => unsusbscribe();
+  }, []);
 
-    useEffect(() => {
-        const unsusbscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser({
-                    uid: user.uid,
-                    email: user.email,
-                    displayName: user.displayName,
-             }) 
-            } else {
-                setUser(null)
-            }
-            setLoading(false)
-        })
-        return () => unsusbscribe();
- },[])
+  const signup = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    
-    const signup = (email: string, password: string) => {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  const login = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const login = (email: string, password: string) => {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
-    
-    const logout = async () => {
-        setUser(null)
-        await signOut(auth)
-    }
-    return (
-        <AuthContext.Provider value={{ user, login, logout, signup}}>
-            {loading ? null : children}
-        </AuthContext.Provider>
-    );
-}
+  const logout = async () => {
+    setUser(null);
+    await signOut(auth);
+  };
+  return (
+    <AuthContext.Provider value={{ user, login, logout, signup }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
