8074724503d4ffcabf499b7f27d830b9b3f611a7
Duvan <duvanserrano21@gmail.com>
2023-08-23 09:01:47 -0500

    Mock de Categorias

---
+++ src/mockData/mockCategory.ts
@@ -0,0 +1,62 @@
+export const category = [
+    {
+     id: '1',
+     name: 'Farolas',
+     image: ['https://i.ebayimg.com/images/g/rcgAAOSw~2Jjhupf/s-l1600.jpg'],
+     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+   },
+   {
+    id: '2',
+    name: 'Stops',
+    image: ['https://i.postimg.cc/hGXcvMz4/H-i3r.jpg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '3',
+    name: 'Exploradoras',
+    image: ['https://grancupon.com/53-large_default/2-exploradoras-de-6-led-para-carros-y-motos.jpg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '4',
+    name: 'Parrillas',
+    image: ['https://lujosyaccesoriostincars.com/wp-content/uploads/2020/12/Portaequipaje-Parrilla.jpg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '5',
+    name: 'Bompers',
+    image: ['https://gruporevisa.net/cdn/shop/collections/bumper-delantero-auto-repuestos-revisa.jpg?v=1660411105'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '6',
+    name: 'Bombillos',
+    image: ['https://voyyo.com.co/uploads/shop/img/901ba4e257c8655a5e949316781206a4.jpeg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '7',
+    name: 'Spoilers',
+    image: ['https://i.pinimg.com/236x/e3/6f/4f/e36f4f00ce2b399210822f5441c6ae36--spoiler-deck.jpg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '8',
+    name: 'Accesorios',
+    image: ['https://images-na.ssl-images-amazon.com/images/I/61-s1ysqZML._AC_UL330_SR330,330_.jpg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '9',
+    name: 'Repuestos',
+    image: ['https://www.pruebaderuta.com/wp-content/uploads/2017/02/repuestos.jpg'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+  {
+    id: '10',
+    name: 'Radios',
+    image: ['https://http2.mlstatic.com/D_NQ_NP_609214-MCO44055199470_112020-O.webp'],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer."
+  },
+]
