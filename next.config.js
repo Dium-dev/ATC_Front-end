c45fe9952e7bd0a95c11caeffd58e9d441b640e3
Clay Martinez <clay.ms@hotmail.com>
2023-08-24 18:07:21 -0500

    Modify next.config to allow it to fetch images from other URLs and use them in the next Image tag. Added some brand cards to the page.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['i.ebayimg.com', 'i.postimg.cc'], // Agrega aquí los dominios permitidos para las imágenes
-    },
-  };
-  
+  images: {
+    domains: [
+      'i.ebayimg.com',
+      'i.postimg.cc',
+      '1000marcas.net',
+      'www.autopista.es',
+      'assets.stickpng.com',
+      'media.gm.com',
+    ], // Agrega aquí los dominios permitidos para las imágenes
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/cards/brandCard.tsx
+++ src/components/cards/brandCard.tsx
@@ -9,17 +9,17 @@ interface CardProps {
 const Card: FC<CardProps> = ({ title, imageSrc }) => {
   return (
     <div className="flex flex-col w-[9.25rem] h-[11.5rem] p-2 group">
-      <div className="w-full h-[138px] overflow-hidden">
+      <div className=" flex items-end w-full h-[138px] overflow-hidden">
         <Image
           src={imageSrc}
-          alt="Cubre Volante"
-          width={147}
-          height={147}
-          className=" rounded-full"
+          alt={`imagen de la marca${title}`}
+          width={130}
+          height={74}
+          className="rounded-full h-[74px] group"
         />
       </div>
 
-      <div className="w-full h-[2.3125rem] overflow-hidden flex items-end justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+      <div className="w-full h-[2.3125rem] overflow-hidden flex items-end justify-center opacity-0 group-hover:opacity-100 transition-opacity group">
         <h3 className="font-oswald font-bold text-xs text-background-lm text-center line-clamp-1 ">
           {title.toUpperCase()}
         </h3>
