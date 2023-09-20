30ff8c51972e50d818470b8de1490eeaaa3e4202
Clay Martinez <clay.ms@hotmail.com>
2023-09-20 10:21:49 -0500

    Reduccion tamaño logo, redes sociales. Ampliacion del margen entre rutas e imagen mercado pago. Linkeo a redes en las imagenes de redes. elimino separador rojo despues de Blog. Añado ruta Blogo al enlace.

--- src/components/footer/ImagesList.tsx
+++ src/components/footer/ImagesList.tsx
@@ -1,28 +1,49 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
-const socialMediaImages = {
-  Instagram: 'https://i.postimg.cc/cLC0Nzb2/instagram.png',
-  Facebook: 'https://i.postimg.cc/XYP3N8MJ/facebook.png',
-  Twitter: 'https://i.postimg.cc/bJxjTfZD/twiter.png',
-  Pinterest: 'https://i.postimg.cc/5tFJXC1m/pinterest.png',
-  Youtube: 'https://i.postimg.cc/V6Q8nkkP/youtube.png',
-};
+const socialMediaImages = [
+  {
+    name: 'Instagram',
+    image: 'https://i.postimg.cc/cLC0Nzb2/instagram.png',
+    route: 'https://instagram.com/',
+  },
+  {
+    name: 'Facebook',
+    image: 'https://i.postimg.cc/XYP3N8MJ/facebook.png',
+    route: 'https://facebook.com/',
+  },
+  {
+    name: 'Twitter',
+    image: 'https://i.postimg.cc/bJxjTfZD/twiter.png',
+    route: 'https://twitter.com/',
+  },
+  {
+    name: 'Pinterest',
+    image: 'https://i.postimg.cc/5tFJXC1m/pinterest.png',
+    route: 'https://pinterest.com/',
+  },
+  {
+    name: 'Youtube',
+    image: 'https://i.postimg.cc/V6Q8nkkP/youtube.png',
+    route: 'https://youtube.com/',
+  },
+];
 
 import React, { FC } from 'react';
 
 interface ImagesListProps {}
 
 const ImagesList: FC<ImagesListProps> = () =>
-  Object.entries(socialMediaImages).map(
-    ([socialMediaName, socialMediaImageUrl]) => (
+  socialMediaImages.map((socialMedia) => (
+    <Link href={socialMedia.route}>
       <Image
-        key={socialMediaName}
-        src={socialMediaImageUrl}
-        width={50}
-        height={50}
+        key={socialMedia.name}
+        src={socialMedia.image}
+        width={40}
+        height={40}
         alt=""
       />
-    )
-  );
+    </Link>
+  ));
 
 export default ImagesList;
