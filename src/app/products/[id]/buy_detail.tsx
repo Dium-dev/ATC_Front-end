39e5fc48ed763f3eb347d3244c9186cc22980f4b
jhoniernem <jhonny30ca@gmail.com>
2023-10-12 19:47:43 -0500

    update

--- src/app/products/[id]/buy_detail.tsx
+++ src/app/products/[id]/buy_detail.tsx
@@ -1,15 +1,19 @@
 export function BuyDetail() {
   return (
     <div>
-      <p>con stock</p>
-      <p>Agregar a favoritos</p>
+      <div className="flex">
+        <p>con stock</p>
+        <p>Agregar a favoritos</p>
+      </div>
       <p>Farola Hyundai Voloster 2012 2018 Drl Led Secuencial Dir Par</p>
       <p>Marca: Hyunday</p>
       <p>Tiempo de entrega: 10 / 15 Días hábiles</p>
       <p>2489999</p>
       <p>2159990</p>
-      <button>Comprar</button>
-      <button>Añadir al carrito</button>
+      <div>
+        <button>Comprar</button>
+        <button>Añadir al carrito</button>
+      </div>
       <p>Envio gratis por compras superiores a $200.000</p>
       <p>¿Tienes alguna pregunta ? consulta con un especialista</p>
       <a href="">Contactanos</a>
