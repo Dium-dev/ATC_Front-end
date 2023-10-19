62e2609159b5c1c588bfcbdff14d85667f4d15e9
Telkens <stivenmg77@gmail.com>
2023-10-19 15:07:46 -0500

    Added fully funcionality with backend request

--- src/components/buyDetail/index.tsx
+++ src/components/buyDetail/index.tsx
@@ -1,36 +1,59 @@
+import { ProductDetail } from '~/types/products';
+import { Brand } from '~/types/products';
 import { GoDotFill } from 'react-icons/go';
 import { AiFillHeart } from 'react-icons/ai';
 import { AiOutlineShopping } from 'react-icons/ai';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import Link from 'next/link';
-export function BuyDetail() {
+import { FC, useEffect, useState } from 'react';
+export const BuyDetail: FC<ProductDetail> = (product) => {
+  const [brand, setBrand] = useState('');
+
+  useEffect(() => {
+    const fetchBrand = async () => {
+      const response = await fetch('http://localhost:3001/brands');
+      const brands = await response.json();
+      const brandFiltered = brands.find(
+        (brand: Brand) => brand.id === product.brandId
+      );
+
+      setBrand(brandFiltered.name);
+    };
+
+    fetchBrand();
+  }, [product.brandId]);
+
   return (
     <div className="w-full h-[79%] my-auto ms:w-5/6 flex flex-col justify-between">
       <div className="flex w-full justify-between">
         <p className="flex items-center text-secondary-lm">
-          <GoDotFill color={'red'} />
-          Sin stock
+          {product.stock > 0 ? (
+            <GoDotFill color={'green'} />
+          ) : (
+            <GoDotFill color={'red'} />
+          )}
+          {product.stock > 0 ? 'Con stock' : 'Sin stock'}
         </p>
         <p className="flex items-center gap-x-1 font-medium">
           Agregar a favoritos <AiFillHeart color={'red'} />
         </p>
       </div>
-      <p className="text-bold text-2xl font-black">
-        Farola Hyundai Voloster 2012 2018 Drl Led Secuencial Dir Par
-      </p>
+      <p className="text-bold text-2xl font-black">{product.title}</p>
       <div className="w-full font-medium">
         <p className="text-primary-lm">Marca:</p>
-        <p>Hyunday</p>
+        <p>{brand}</p>
       </div>
-      <div className='w-full font-medium'>
+      <div className="w-full font-medium">
         <p className="text-primary-lm">Tiempo de entrega:</p>
         <p>10 / 15 Días hábiles</p>
       </div>
       <div className="w-full">
         <p className="line-through text-xl text-primary-dm font-extralight">
-          $2489999
+          ${product.price - product.price * 0.1}
         </p>
-        <p className="text-2xl text-secondary-lm font-black">$2159990</p>
+        <p className="text-2xl text-secondary-lm font-black">
+          ${product.price}
+        </p>
       </div>
       <div className="w-full flex justify-between">
         <button className="w-[49%] bg-secondary-lm text-white rounded p-1 flex items-center justify-center gap-x-1 font-bold">
@@ -42,11 +65,13 @@ export function BuyDetail() {
           <AiOutlineShoppingCart size={25} />
         </button>
       </div>
-      <p className='font-light'>Envio gratis por compras superiores a $200.000</p>
+      <p className="font-light">
+        Envio gratis por compras superiores a $200.000
+      </p>
       <p>¿Tienes alguna pregunta? Consulta con un especialista</p>
       <Link href={'/contact'} className="text-primary-lm">
         Contactanos
       </Link>
     </div>
   );
-}
+};
