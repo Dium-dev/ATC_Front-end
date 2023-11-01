94f09cbeb7a69c84140f66b19e16a1fe6b558d1e
Telkens <stivenmg77@gmail.com>
2023-11-01 10:39:44 -0500

    Add black background in banner and fix number of categories in only row

--- src/components/cards/categoryCard.tsx
+++ src/components/cards/categoryCard.tsx
@@ -3,7 +3,7 @@ import { CategoryProps } from '~/types/products';
 
 const CardCategory: React.FC<CategoryProps> = ({ name, image }) => {
   return (
-    <div className="flex w-[10rem] h-[200px] flex-col items-center overflow-hidden m-8">
+    <div className="flex w-[10rem] h-[200px] flex-col items-center overflow-hidden my-1">
       <div className="w-full h-[78%] relative">
         <Image src={image} alt="Imagen de Categoria" layout="fill" />
       </div>
