60ec2dc7d3ba5e4dd88f55f7296b0de9085690d0
Telkens <stivenmg77@gmail.com>
2023-11-05 16:50:12 -0500

    Fixed duplicate style and img element changed by Image component

--- src/components/componetsDashboard/Cards/CardProfile.jsx
+++ src/components/componetsDashboard/Cards/CardProfile.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 // components
@@ -10,9 +11,9 @@ export default function CardProfile() {
           <div className="flex flex-wrap justify-center">
             <div className="w-full px-4 flex justify-center">
               <div className="relative">
-                <img
+                <Image
                   alt="..."
-                  src={require("~/assets/img/team-2-800x800.jpg").default}
+                  src={require('~/assets/img/team-2-800x800.jpg').default}
                   className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
                 />
               </div>
@@ -41,7 +42,7 @@ export default function CardProfile() {
             </div>
           </div>
           <div className="text-center mt-12">
-            <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+            <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700">
               Jenna Stones
             </h3>
             <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
