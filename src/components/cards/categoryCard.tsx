811e3e959f399947efb0fccd187c40a8e2b2fec3
Telkens <stivenmg77@gmail.com>
2023-11-05 16:47:49 -0500

    Fixed error in console browser due to missing of definition of size

--- src/components/cards/categoryCard.tsx
+++ src/components/cards/categoryCard.tsx
@@ -5,7 +5,7 @@ const CardCategory: React.FC<CategoryProps> = ({ name, image }) => {
   return (
     <div className="flex w-[10rem] h-[200px] flex-col items-center overflow-hidden my-1">
       <div className="w-full h-[78%] relative">
-        <Image src={image} alt="Imagen de Categoria" layout="fill" />
+        <Image src={image} alt="Imagen de Categoria" width={300} height={300}/>
       </div>
       <div>
         <h3 className="font-bold text-2xl w-[100%] h-[22%] mt-2">{name}</h3>
