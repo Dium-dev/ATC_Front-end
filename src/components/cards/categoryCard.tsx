dcda6d15b1813f05093310cd2034d02aa5bc9670
Telkens <stivenmg77@gmail.com>
2023-11-06 16:22:45 -0500

    Added funcionality to banner categorias and navbar categories

--- src/components/cards/categoryCard.tsx
+++ src/components/cards/categoryCard.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image';
+import { useProductStore } from '~/store/productStore';
 import { CategoryProps } from '~/types/products';
+import {useRouter} from 'next/navigation'
 
-const CardCategory: React.FC<CategoryProps> = ({ name, image }) => {
+const CardCategory: React.FC<CategoryProps> = ({ name, image, id }) => {
+  const router = useRouter()
+  const updateBody = useProductStore(state =>  state.updateBody);
+  const handleClick = () => {
+    updateBody('categoryId', id)
+    router.push('/products')
+  }
+
   return (
-    <div className="flex w-[10rem] h-[200px] flex-col items-center overflow-hidden my-1">
+    <div className="flex w-[10rem] h-[200px] flex-col items-center overflow-hidden my-1" onClick={handleClick}>
       <div className="w-full h-[78%] relative">
         <Image src={image} alt="Imagen de Categoria" width={300} height={300}/>
       </div>
