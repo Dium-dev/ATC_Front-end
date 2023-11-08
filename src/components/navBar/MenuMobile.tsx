0633ccc41b0c06f4ca648ef9197b9e20913bb0b8
Telkens <stivenmg77@gmail.com>
2023-11-08 15:37:47 -0500

    Al acceder al blog desde el menuMobile se me abre en una pestaña nueva

--- src/components/navBar/MenuMobile.tsx
+++ src/components/navBar/MenuMobile.tsx
@@ -26,6 +26,7 @@ const MenuMobile: FC<MenuMobileProps> = ({ updateState }) => {
             key={item.url}
             href={item.url}
             className="block p-4 transform  hover:scale-125"
+            target='_blank'
           >
             {item.label}
           </Link>
