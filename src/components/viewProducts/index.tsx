e0046b99f64d1cb9dff479540632d8bc48812497
Telkens <stivenmg77@gmail.com>
2023-11-21 12:04:07 -0500

    Fix background color in filters container

--- src/components/viewProducts/index.tsx
+++ src/components/viewProducts/index.tsx
@@ -7,7 +7,7 @@ const ViewProducts: FC = () => {
   return (
     <section className="w-full flex flex-col items-start justify-between md:gap-x-5 gap-y-6 md:gap-y-0 mt-20 md:mt-0">
       <div className='flex-col md:flex-row flex min-h-max'>
-        <div className='flex flex-col items-start w-full md:w-1/4 p-10 min-h-max md:bg-gray md:pt-10 pt-20 dark:bg-secondary-dm'>
+        <div className='flex flex-col items-start w-full md:w-1/4 p-10 min-h-max md:bg-gray md:pt-10 pt-20 dark:bg-[#252525]'>
             <Filters />
         </div>
         <ContainerProducts />
