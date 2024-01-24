0f5927d85106acc75b8f63ca8dfc8bbd84e0a3d7
SourerDev <jhonny30ca@gmail.com>
2024-01-24 17:54:46 -0500

    Fix view component wrong position render

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+import ViewProducts from '~/components/viewProducts';
 import { ContainerPage } from '../container_page';
 
-export default function Landing() {
+export default function ProductsPage() {
   return <ContainerPage>
+    <ViewProducts />
   </ContainerPage>;
 }
