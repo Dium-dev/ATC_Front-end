16d7290385af2fb7be0ff2f41cb37e5a27dad208
SourerDev <jhonny30ca@gmail.com>
2024-01-24 20:12:29 -0500

    Implement example use services

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,23 @@
-'use client'
+'use client';
 import ViewProducts from '~/components/viewProducts';
 import { ContainerPage } from '../container_page';
+import { useEffect } from 'react';
+import { api } from 'services';
 
 export default function ProductsPage() {
-  return <ContainerPage>
-    <ViewProducts />
-  </ContainerPage>;
+  useEffect(() => {
+    api
+      .getProducts({})
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
+  }, []);
+  return (
+    <ContainerPage>
+      <ViewProducts />
+    </ContainerPage>
+  );
 }
